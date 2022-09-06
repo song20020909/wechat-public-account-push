7e08eec93fdbc1aa4ff1058b3498b987edf70550
song20020909 <112911767+song20020909@users.noreply.github.com>
2022-09-06 09:11:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -37,11 +37,11 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "广东",
+      province: "河北",
       // 所在城市
-      city: "肇庆",
+      city: "保定",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      
     }, 
     {
       name: "老婆1", 
